1394c43fb0e87c5ae689fd2ebc2f4ef15b14c64c
Tracey Birch <tracey.birch@sofwerx.org>
2018-05-18 15:52:10 -0400

    mission plan object

--- glo.js
+++ glo.js
@@ -10,6 +10,26 @@ var viewer = new Cesium.Viewer('cesiumContainer',{
   })
 });
 
+function MissionPlan(name) {
+  Object.defineProperty(this, "name", {
+    get: function() {
+        return name;
+    },
+    set: function(newName) {
+        name = newName;
+    },
+    enumerable: true,
+    configurable: true
+  });
+
+  this.name = name;
+
+  this.logName = function() {
+    console.log(this.name);
+  };
+}
+
+
 var center = Cesium.Cartesian3.fromDegrees(-82.5, 35.3);
 viewer.camera.lookAt(center, new Cesium.Cartesian3(0.0, 0.0, 20000000.0));
 
@@ -17,18 +37,29 @@ console.log("Cesium Initialized");
 
 var missionPlanningDialog = document.querySelector('#mission-planning-dialog');
 var missionPlanningLink = document.querySelector('#mission-planning-link');
+var curMissionPlan = new MissionPlan("ENTER MISSION NAME");
+
 if (! missionPlanningDialog.showModal) {
   console.log("Registering missionPlanningDialog");
   dialogPolyfill.registerDialog(missionPlanningDialog);
 }
-missionPlanningLink.addEventListener('click', function() {
-  console.log("Showing missionPlanningDialog");
-  missionPlanningDialog.showModal();
-});
-missionPlanningDialog.querySelector('.close').addEventListener('click', function() {
+
+closeBtn = missionPlanningDialog.querySelector('.close');
+closeBtn.addEventListener('click', function() {
   console.log("Closing missionPlanningDialog");
   missionPlanningDialog.close();
 });
+missionPlanningDialog.querySelector('#missionName').addEventListener('change', function() {
+  curMissionPlan.name = missionPlanningDialog.querySelector('#missionName').value;
+});
+
+missionPlanningLink.addEventListener('click', function() {
+  console.log("Showing missionPlanningDialog");
+  mname = missionPlanningDialog.querySelector('#missionName');
+  mname.value = curMissionPlan.name;
+  missionPlanningDialog.showModal();
+});
+
 
 var phase1Dialog = document.querySelector('#phase1-dialog');
 var phase1Link = document.querySelector('#phase1-link');
@@ -38,9 +69,11 @@ if (! phase1Dialog.showModal) {
 }
 phase1Link.addEventListener('click', function() {
   console.log("Showing phase1Dialog");
+  var mnHdg = phase1Dialog.querySelector('#mission-name-heading');
+  mnHdg.innerText = curMissionPlan.name;
   phase1Dialog.showModal();
 });
 phase1Dialog.querySelector('.close').addEventListener('click', function() {
   console.log("Closing phase1Dialog");
   phase1Dialog.close();
-}); 
+});
