e249e192592f99118679bc6d3b62c3d8456df3f2
Tracey Birch <tracey.birch@sofwerx.org>
2018-05-19 18:25:52 -0400

    Mission name, dates, location persist. Duration calculated. UI tweaks.

--- glo.js
+++ glo.js
@@ -15,6 +15,14 @@ var viewer = new Cesium.Viewer('cesiumContainer',{
 const MGRS_PRECISION = 3;
 
 function MissionPlan(name) {
+
+  // Phase I -- these should go in an array of phase objs eventually
+  this.cartographicPosition = null;
+  this.mgrs = null;
+  this.icon = null;
+  this.startDate = new Date();
+  this.duration = 0;
+
   Object.defineProperty(this, "name", {
     get: function() {
         return name;
@@ -26,10 +34,19 @@ function MissionPlan(name) {
     configurable: true
   });
 
+  Object.defineProperty(this, "endDate", {
+    get: function() {
+        return endDate;
+    },
+    set: function(newDate) {
+        endDate = newDate;
+    },
+    enumerable: true,
+    configurable: true
+  });
+
   this.name = name;
-  this.cartographicPosition = null;
-  this.mgrs = null;
-  this.cesiumBB = null;
+  this.endDate = new Date();
 
   this.logName = function() {
     console.log(this.name);
@@ -45,7 +62,7 @@ function MissionPlan(name) {
   }
 }
 
-var curMissionPlan = new MissionPlan("ENTER MISSION NAME");
+var curMissionPlan = new MissionPlan("");
 
 var scene = viewer.scene;
 var handler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
@@ -63,11 +80,11 @@ handler.setInputAction(function(click) {
 
     curMissionPlan.cartographicPosition = cartographicPosition;
     curMissionPlan.mgrs = mgrs_str;
-    if (curMissionPlan.cesiumBB) {
-      curMissionPlan.cesiumBB.destroy();
+    if (curMissionPlan.icon) {
+      curMissionPlan.icon.destroy();
     }
-    curMissionPlan.cesiumBB = scene.primitives.add(new Cesium.BillboardCollection());
-    curMissionPlan.cesiumBB.add({
+    curMissionPlan.icon = scene.primitives.add(new Cesium.BillboardCollection());
+    curMissionPlan.icon.add({
       position: position,
       scale: 0.25,
       image: 'red_marker.png',
@@ -97,6 +114,8 @@ closeBtn.addEventListener('click', function() {
 missionPlanningDialog.querySelector('#missionName').addEventListener('change', function() {
   curMissionPlan.name = missionPlanningDialog.querySelector('#missionName').value;
 });
+var mStart =  missionPlanningDialog.querySelector('#missionStartDate');
+var mEnd = missionPlanningDialog.querySelector('#missionEndDate');
 
 function beginMissionPlan() {
     var d = document.querySelector('.mdl-layout__drawer.is-visible');
@@ -118,6 +137,9 @@ function beginMissionPlan() {
     var locField = missionPlanningDialog.querySelector('#missionLocation');
     locField.value = curMissionPlan.formatMGRS();
 
+    mStart.value = curMissionPlan.startDate.toLocaleDateString();
+    mEnd.value = curMissionPlan.endDate.toLocaleDateString();
+
     mname = missionPlanningDialog.querySelector('#missionName');
     mname.value = curMissionPlan.name;
     mname.select();
@@ -128,25 +150,51 @@ function beginMissionPlan() {
 missionPlanningLink.addEventListener('click', beginMissionPlan);
 
 
+mStart.addEventListener('change', function() {
+  [m, d, y] = mStart.value.split('/');
+  curMissionPlan.startDate = new Date(y, m-1, d, 0, 0, 0);
+  // curMissionPlan.startDate.setYear(y);
+  // curMissionPlan.startDate.setMonth(m-1);
+  // curMissionPlan.startDate.setDate(d);
+});
+mEnd.addEventListener('change', function() {
+  [m, d, y] = mEnd.value.split('/');
+  curMissionPlan.endDate = new Date(y, m-1, d, 0, 0, 0);
+  // curMissionPlan.endDate.setYear(y);
+  // curMissionPlan.endDate.setMonth(m-1);
+  // curMissionPlan.endDate.setDate(d);
+});
+
 var phase1Dialog = document.querySelector('#phase1-dialog');
 var phase1Link = document.querySelector('#phase1-link');
 if (! phase1Dialog.showModal) {
   console.log("Registering phase1Dialog");
   dialogPolyfill.registerDialog(phase1Dialog);
 }
+var mStart1 = phase1Dialog.querySelector('#missionStartDate');
+var mEnd1 = phase1Dialog.querySelector('#missionEndDate');
+
 
 function planPhaseOne() {
+  mStart1.innerText = curMissionPlan.startDate.toLocaleDateString();
+  mEnd1.innerText = curMissionPlan.endDate.toLocaleDateString();
+
   console.log("Showing phase1Dialog");
   var mnHdg = phase1Dialog.querySelector('#mission-name-heading');
   mnHdg.innerText = curMissionPlan.name;
   var locField = phase1Dialog.querySelector('#missionLocation');
   locField.innerText = curMissionPlan.formatMGRS();
+  var durationField = phase1Dialog.querySelector('#missionDuration');
+  curMissionPlan.duration = Math.floor((curMissionPlan.endDate - curMissionPlan.startDate) / (1000*60*60*24));
+  durationField.innerText = curMissionPlan.duration;
   phase1Dialog.showModal();
+  missionPlanningDialog.close();
 };
 
 phase1Link.addEventListener('click', planPhaseOne);
 
 phase1Dialog.querySelector('.close').addEventListener('click', function() {
   console.log("Closing phase1Dialog");
+  missionPlanningDialog.showModal();
   phase1Dialog.close();
 });
