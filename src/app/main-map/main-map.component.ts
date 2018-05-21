aef2e8affac130f655b147212d6e369271780171
eitanfr <eitan.fr5@gmail.com>
2018-05-21 09:17:06 +0300

    initial form

--- src/app/main-map/main-map.component.ts
+++ src/app/main-map/main-map.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewerConfiguration } from "angular-cesium";
 
 @Component({
   selector: 'main-map',
   templateUrl: './main-map.component.html',
-  styleUrls: ['./main-map.component.css']
+  styleUrls: ['./main-map.component.css'],
+  providers: [ViewerConfiguration],
 })
 export class MainMapComponent implements OnInit {
 
-  constructor() { }
+  constructor(private viewrConf: ViewerConfiguration) {
+    viewrConf.viewerOptions = {
+      selectionIndicator : false,
+      timeline : false,
+      infoBox : false,
+      animation : false,
+      navigationHelpButton : false,
+      navigationInstructionsInitiallyVisible : false,
+    };
+  }
 
   ngOnInit() {
   }
